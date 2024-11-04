5d7113e5b10e1452bdcc09d24ba05348afa37a95
MartaCanino <martacaninoromero@gmail.com>
2024-11-04 17:13:33 +0100

    Generar contraseñasa opcion1  con math random

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
-# Ejercicios relacionados con NodeJS para la asignatura de Sistemas Web I
-## Ejercicio 3
+# Ejercicios SWI
+Ejercicios relacionados con NodeJS para la asignatura de Sistemas Web I
+
+## Ejercicio 3
+
 - Crea un servidor en node.js
 - Que al cargar la página principal muestre una contraseña aleatoria
 - Generada a partir de X palabras aleatorias seleccionadas de un diccionario
 - El número de palabras (X) está definido como parámetro en la query
-    Accesible en req.url
+    - Accesible en req.url
+
+## Ejercicio 4
+
+Web scraping
+- Extraer datos de páginas web
+- De forma automática haciendo uso de bots
+- Una vez extraída la información se procesa
+- Se repite el proceso a lo largo del tiempo para comparar la evolución de los datos
+- Ejemplos:
+    - Google extrayendo información de las webs para el buscador
+    - Internet Archive para conservar webs antiguas
+    - Páginas de comparación de precios
+- Crea un servidor
+    - Inicializa package.json
+    - Que descargue periódicamente el HTML de una web
+    - Que lo procese para extraer una información específica
+    - Opcional: usa algún paquete como cheerio para manipular el DOM
+- Cuidado
+    - Algunas webs requieren JS para visualizarse
+    - No hagas muchas peticiones en poco tiempo
---
+++ ejercicio_3/opcion1/index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const port = 3000;
+const url = require('url');
+
+const palabras = require('./palabras.json'); // accedemos el diccionario con palabras aleatorias
+
+// función para obtener una palabra aleatoria del diccionario
+function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * palabras.length);
+    return palabras[randomIndex];
+}
+
+// función para generar una contraseña de X palabras
+function generatePassword(numWords) {
+    let password = [];
+    for (let i = 0; i < numWords; i++) {
+        password.push(getRandomWord());
+    }
+    return password.join('-'); // Separa las palabras con un guion
+}
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const query = parsedUrl.query;
+
+    // función el valor de 'X' desde los parámetros de consulta o usa 2 como valor por defecto
+    const X = parseInt(query.X, 10) || 2;
+
+    // función la contraseña aleatoria de X palabras
+    const password = generatePassword(X);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Password generated from ${X} ranadom words: ${password}\n`);
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
